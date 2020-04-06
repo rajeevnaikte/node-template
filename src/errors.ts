f249ace1f498f4af550802fb78219746ff86230b
rajeevnaikte <rajeevnaikte@gmail.com>
2020-04-05 22:46:48 -0400

    import base error from squid-utils

--- src/errors.ts
+++ src/errors.ts
@@ -1,12 +1,1 @@
-class BaseError extends Error {
-  readonly code: string;
-
-  constructor (code: string, message: string) {
-    super(message);
-    this.code = code;
-  }
-
-  toString (): string {
-    return `${this.code}: ${this.message}`;
-  }
-}
+import { BaseError } from 'squid-utils';
